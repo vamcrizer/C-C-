153279a3587be0c9cb9cdbdf5f7d94848b1ec9d3
vamcrizer <vamcrizermcgaming@gmail.com>
2023-10-13 13:16:41 +0700

    update

---
+++ es.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  char c[20];
+  char s[20];
+  scanf("%s", c);
+  scanf("%s", s);
+  int maximum = 0;
+  for (int i = 0; i < strlen(c); i++){
+
+  }
+  return 0;
+}
