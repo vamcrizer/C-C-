5545a953ba79e116b384b714c0d5fe1574e833a2
Vũ Hoàng Anh <142197061+vamcrizer@users.noreply.github.com>
2023-10-14 20:54:47 +0700

    Create EC04014.c

--- es.c
+++ es.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-int main(){
-  char c[20];
-  char s[20];
-  scanf("%s", c);
-  scanf("%s", s);
-  int maximum = 0;
-  for (int i = 0; i < strlen(c); i++){
 
-  }
-  return 0;
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++){
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++){
+            scanf("%d", &arr[i]);
+        }
+        int dem = 1;
+        int count = 0;
+        int res = 0;
+        int b[n];
+        for (int i = 1; i < n; i++){
+            if (arr[i] > arr[i - 1]){
+                ++dem;
+            } else {
+                dem = 1;
+            }
+            if (dem > res){
+                res = dem;
+                b[0] = i - res + 1; 
+                count = 1;
+            } else if (dem == res){
+                b[count] = i - res + 1;
+                ++count;
+            }
+        }
+        printf("Test %d:\n", i);
+        printf("%d\n", res);
+        
+        for (int i = 0; i < count; i++){
+            for (int j = 0; j < res; j++){
+                printf("%d ", arr[b[i] + j]);
+                }
+            printf("\n");
+            }
+            
+    }
+    return 0;
 }
