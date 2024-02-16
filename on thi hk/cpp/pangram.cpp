113dec8647b7c1e058cf382c8bad895aa03cfefa
Vũ Hoàng Anh <142197061+vamcrizer@users.noreply.github.com>
2024-02-16 22:48:28 +0700

    Update pangram.cpp

--- on thi hk/cpp/pangram.cpp
+++ on thi hk/cpp/pangram.cpp
@@ -8,11 +8,22 @@ int main(){
     cin.tie(NULL);cout.tie(NULL);
     int t; cin >> t;
     while(t--){
-        int a[200] = {0};
+        int x[200] = {0};
         string a; cin >> a;
-        int x; cin >> x;
+        int k; cin >> k;
         for (int i = 0; i < a.length(); i++){
             x[a[i]]++;
+        }
+        int cnt = 0;
+        for (int i = 97 ; i <= 122; i++){
+            if (x[i] == 0){
+                cnt++;
+            }
+        }
+        if (k >= cnt){
+            cout << 1 << endl;
+        } else {
+            cout << 0 << endl;
         }
     }
 }
