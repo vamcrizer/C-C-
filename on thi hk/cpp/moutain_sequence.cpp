8252ac842e9db8e677b5019ed157fe6e75b0ec52
Vũ Hoàng Anh <142197061+vamcrizer@users.noreply.github.com>
2024-02-18 21:25:21 +0700

    Add files via upload

--- on thi hk/cpp/moutain_sequence.cpp
+++ on thi hk/cpp/moutain_sequence.cpp
@@ -1,48 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const int MOD = (int)1e9 + 7;
+const int MOD = (int)1e7;
 
 string check(int a[], int n, int l, int r){
-    int increase = 0;
-    if (a[l] > a[l+1]){
-        return "No\n";
-    } else if (a[l] < a[l + 1]) {
-        increase = 1;
-    }
-    int ok = 0;
-    for (int i = l + 1; i <= r; i++){
-        if (a[i] < a[i - 1] || a[i] > a[i - 1]){
-            ok = 1;
-        }
-        if (a[i] > a[i - 1] && a[i - 1] == a[i - 2]){
-            increase = 1;
-        }
-        if (a[i] < a[i - 1] && a[i - 1] == a[i - 2]){
-            return "No\n";
-        }
-        if (a[i] < a[i - 1] && increase == 1){
-            increase = 0;
-        }
-        if (a[i] > a[i - 1] && increase == 0){
+    auto k = max_element(a + l, a + r);
+    int pos = distance(a, k);
+    for (int i = l + 1; i < pos; i++){
+        if (a[i] < a[i - 1]){
             return "No\n";
         }
     }
-    if (!ok){
-        return "No\n";
+    for (int i = pos + 1; i <= r; i++){
+        if (a[i] > a[i - 1]){
+            return "No\n";
+        }
     }
     return "Yes\n";
 }
 
 int main(){
+    ios_base::sync_with_stdio(0); 
+    cin.tie(NULL);cout.tie(NULL);
     int t; cin >> t;
     while(t--){
-    int n; cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++){
-        cin >> a[i];
-    }
-    int l, r; cin >> l >> r;
-    cout << check(a, n, l, r);
+        int n; cin >> n;
+        int a[n]; for (auto &x : a) cin >> x;
+        int l, r; cin >> l >> r;
+        cout << check(a, n, l, r) << endl;
     }
 }
