cb522e98450de201cc9d0a4b79c74691c464f829
Vũ Hoàng Anh <142197061+vamcrizer@users.noreply.github.com>
2024-02-20 14:35:29 +0700

    commit

--- on thi hk/cpp/fraction_operations.cpp
+++ on thi hk/cpp/fraction_operations.cpp
@@ -3,28 +3,28 @@ using namespace std;
 using ll = long long;
 int T;
 
-int lcm(int a, int b){
+ll lcm(ll a, ll b){
     return a / __gcd(a, b) * b;
 }
 
 struct Fraction{
-    int tu;
-    int mau;
+    ll tu;
+    ll mau;
 };
 typedef struct Fraction Fraction;
 void read_input(Fraction &a){
     cin >> a.tu >> a.mau;
 }
-void process (Fraction a, Fraction b){
-    int mcnn = lcm(a.mau, b.mau);
+void process(Fraction a, Fraction b){
+    ll mcnn = lcm(a.mau, b.mau);
     a.tu = a.tu * mcnn / a.mau;
     b.tu = b.tu * mcnn / b.mau;
     a.mau = mcnn;
     b.mau = mcnn;
-    int tuso, mauso;
+    ll tuso, mauso;
     tuso = (a.tu + b.tu)*(a.tu + b.tu);
     mauso = mcnn*mcnn;
-    int gcd = __gcd(tuso, mauso);
+    ll gcd = __gcd(tuso, mauso);
     cout << tuso / gcd << "/" << mauso / gcd << " ";
     gcd = __gcd(tuso * a.tu * b.tu, a.mau * b.mau * mauso);
     cout << tuso * a.tu * b.tu / gcd << "/" << a.mau * b.mau * mauso / gcd;
