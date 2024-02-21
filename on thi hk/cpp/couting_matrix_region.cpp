301b4e1e0edf33fb5194b4f868a565f9f4c57b0e
Vũ Hoàng Anh <142197061+vamcrizer@users.noreply.github.com>
2024-02-21 21:55:45 +0700

    commit

--- on thi hk/cpp/couting_matrix_region.cpp
+++ on thi hk/cpp/couting_matrix_region.cpp
@@ -1,83 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+ll a[105][105];
+
+int n, m;
+int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+void loang(int i, int j){
+    a[i][j] = 0;
+    for (int k = 0; k < 8; k++){
+        int i1 = i + dx[k], j1 = j + dy[k];
+        if (i1 >= 1 && i1 <= n && j1 >= 1 && j1 <= m){
+            if (a[i1][j1]){
+                loang(i1, j1);
+            }
+        }
+    }
+}
 
 int main(){
-int t; cin >> t;
-while(t--){
-
-    int rows, cols;
-
-    cin >> rows >> cols;
-
-    int matrix[rows][cols];
-    queue<pair<int, int>> q;
-    vector<pair<int, int>> v;
-
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        {
-            char tmp;
-            cin >> tmp;
-            
-            if (tmp == 'O')
-            {
-                matrix[i][j] = 0;
-            }
-            else
-            {
-                matrix[i][j] = 1;
-            }
-
-            if (matrix[i][j] == 0 && (i == 0 || j == 0 || i == rows - 1 || j == cols - 1))
-            {
-                q.push({i, j});
-                matrix[i][j] = -1;
+    ios_base::sync_with_stdio(0); 
+    cin.tie(NULL);cout.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            cin >> a[i][j];
+        }
+    }
+    int cnt = 0;
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            if(a[i][j]){
+                cnt++;
+                loang(i,j);
             }
         }
     }
-
-    while (!q.empty())
-    {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        v.push_back({x, y});
-
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
-        for (int k = 0; k < 4; ++k)
-        {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-
-            if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && matrix[nx][ny] == 0)
-            {
-                q.push({nx, ny});
-                matrix[nx][ny] = -1;
-            }
-        }
+    
+    cout << cnt << endl;
     }
-
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        {
-            if (matrix[i][j] == -1)
-            {
-                cout << "O" << " ";
-            }
-            else
-            {
-                cout << "X" << " ";
-            }
-        }
-        cout << "\n";
-    }
-    cout << endl;
-}
-    return 0;
 }
